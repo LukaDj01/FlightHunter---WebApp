b5b659032bcabcc1be9ebce5161056e5b5cdcb59
bukumira <bukumira@elfak.rs>
2024-01-04 19:01:12 +0100

    Dodate View klase

---
+++ FHLibrary/DTOsNeo/LuggageView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHLibrary.DTOsNeo
+{
+    internal class LuggageView
+    {
+        public string? number { get; set; }
+        public float? weight { get; set; }
+        public float? dimension { get; set; }
+        public float? pricePerKG { get; set; }
+
+        public LuggageView() { }
+
+        internal LuggageView(Luggage? l)
+        {
+            if (l != null)
+            {
+                number = l.number;
+                weight = l.weight;
+                dimension = l.dimension;
+                pricePerKG = l.pricePerKG;
+            }
+        }
+    }
+}
---
+++ FHLibrary/DTOsNeo/PlaneView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHLibrary.DTOsNeo
+{
+    internal class PlaneView
+    {
+        public string? serialNumber { get; set; }
+        public string? fuel { get; set; }
+        public string? type { get; set; }
+        public PlaneView() { }
+
+        internal PlaneView(Plane? p)
+        {
+            if (p != null)
+            {
+                serialNumber = p.serialNumber;
+                fuel = p.fuel;
+                type = p.type;
+            }
+        }
+    }
+}
