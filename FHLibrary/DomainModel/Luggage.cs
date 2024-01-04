63724013decd1b4aba9a12beac329aa6ded7a7d2
bukumira <bukumira@elfak.rs>
2024-01-04 13:53:26 +0100

    Dodate su klase Luggage.c i Plane.cs i kontoleri za iste

---
+++ FHLibrary/DomainModel/Luggage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHLibrary.DomainModel
+{
+    internal class Luggage
+    {
+        internal protected virtual string? number { get; set; }
+        internal protected virtual float? weight { get; set; }
+        internal protected virtual float? dimension { get; set; }
+        internal protected virtual float? pricePerKG { get; set; }
+
+        internal Luggage() { }
+    }
+}
---
+++ FlightHunter/Controllers/LuggageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FHLibrary;
+using FHLibrary.DTOsNeo;
+namespace FlightHunter.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class LuggageController : ControllerBase
+{
+    [HttpPost]
+    [Route("AddLuggage")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> AddLuggage([FromBody] LuggageView luggageView)
+    {
+        if (luggageView == null)
+        {
+            return BadRequest("Invalid input data");
+        }
+
+        var result = await Neo4JDataProvider.AddLuggage(luggageView);
+
+        if (result.IsError)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok("Uspešno");
+    }
+}
