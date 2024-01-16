e635d8b2c6071324f35e0562fdceec5473700914
LukaDj01 <lukaluxy01@gmail.com>
2024-01-16 01:50:09 +0100

    radi

--- Client/js/login-registration.js
+++ Client/js/login-registration.js
@@ -3,61 +3,64 @@ import { Passenger } from "./Passenger.js";
     let RegisterBtn = document.querySelector(".addPassenger");
     RegisterBtn.addEventListener("click", function(){
             let name = document.querySelector(".name").value;
-            if (!validateField(name, "name")) {
+            if (name == "") {
+                console.log("Izaberite dolazni gate");
                 return;
             }
-            let surname = document.getElementById(".surname").value;
-            if (!validateField(surname, "surname")) {
+            let surname = document.querySelector(".surname").value;
+            if (surname == "") {
+                console.log("Izaberite dolazni gate");
                 return;
             }
-            let email = document.getElementById(".email").value;
-            if (!validateField(email, "email")) 
+            let email = document.querySelector(".email").value;
+            if (email == "") {
+                console.log("Izaberite dolazni gate");
                 return;
-
-            let password = document.getElementById(".password").value;
+            }
+            let password = document.querySelector(".password").value;
             if(password == "")
             {
                 console.log("Izaberite dolazni gate");
                 return;
             }
-            let nationality = document.getElementById(".nationality").value;
+            let nationality = document.querySelector(".nationality").value;
             if(nationality == "")
             {
                 console.log("Izaberite kapacitet");
                 return;
             }
 
-            let phone = document.getElementById(".phone").value;
+            let phone = document.querySelector(".phone").value;
             if(phone == "")
             {
                 console.log("Izaberite kapacitet");
                 return;
             }
-            let birthday = document.getElementById(".birthday").value;
+            let birthday = document.querySelector(".birthday").value;
             if(birthday == "")
             {
                 console.log("Izaberite kapacitet");
                 return;
             }
-            let passport = document.getElementById(".passport").value;
+            let passport = document.querySelector(".passport").value;
             if(passport == "")
             {
                 console.log("Izaberite kapacitet");
                 return;
             }
-            let street = document.getElementById(".street").value;
+            let street = document.querySelector(".street").value;
             if(street == "")
             {
                 console.log("Izaberite kapacitet");
                 return;
             }
-            let stnumber = document.getElementById(".stnumber").value;
+            let stnumber = document.querySelector(".stnumber").value;
             if(stnumber == "")
             {
                 console.log("Izaberite kapacitet");
                 return;
             }
-            console.log("OCem");
+            console.log(name, surname);
 
         /*fetch(`http://localhost:5163/Passenger/AddPassenger/`, {
 		method: "POST",
@@ -76,13 +79,13 @@ import { Passenger } from "./Passenger.js";
             street:street,
             stnumber:stnumber
 		})
-	}).then(p=>{
-		if(p.ok){
-			location.reload();
-		}
-		else
-		{
-			console.log("nesto je poslo po zlu iks de");
-		}
-	}).catch(errorMsg=>console.log(errorMsg));*/
+        }).then(p=>{
+            if(p.ok){
+                location.reload();
+            }
+            else
+            {
+                console.log("nesto je poslo po zlu iks de");
+            }
+        }).catch(errorMsg=>console.log(errorMsg));*/
 });
